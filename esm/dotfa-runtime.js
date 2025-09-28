5d09672dc28b74c7127791adfb0fd1f76f5935da
pvckstein <160131995+pvckstein@users.noreply.github.com>
2025-09-28 12:29:10 +0200

    Update dotfa-runtime.js

--- esm/dotfa-runtime.js
+++ esm/dotfa-runtime.js
@@ -13,6 +13,10 @@ import { renderCustom }                 from './modes/custom.js';
 import { renderFetch, renderFetchData } from './modes/fetch.js';
 import { renderPage }                   from './modes/page.js';
 
+// componentes
+
+import './components/popover.js';
+
 // --------- helpers internos ---------
 const tplCache = new Map(); // tplId -> compiled function
 
